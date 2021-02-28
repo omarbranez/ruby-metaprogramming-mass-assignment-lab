849c76bf64cb95443f925ed033be31c274de4e45
Omar Branez <omar.branez@gmail.com>
2021-02-27 19:28:39 -0500

    commit tree 6021aa86a868b1c28db3048419f1ff64c52ea3a1

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+    def initialize(attributes)
+      attributes.each do |key, value| #iterate on the incoming attributes, mass assign as a hash (thus the key and value) 
+        self.class.attr_accessor(key) #for that instance, create new attr_accessors for each key. attr_accessor is a class method
+        self.send(("#{key}="), value) #for as many keys(attributes) exist, _SEND_ will initialize a new instance while linking them to the value
+      end
+    end
 end
